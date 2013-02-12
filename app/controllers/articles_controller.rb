6f268166930c8252f09fe6df02732ec05f3af80e
Tim <tmmtsmith@gmail.com>
2013-02-12 06:14:05 -0600

    Step Create New Post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,13 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+	def new
+		@article = Article.new
+	end
+	def create
+	  @article = Article.new
+	  @article.title = params[:article][:title][:body]
+	  @article.save
+	  redirect_to article_path(@article)
+	end
 end
