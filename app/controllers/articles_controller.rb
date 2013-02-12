52bcfa5f9671d160a0428b22a2e4249783ca3f89
Tim <tmmtsmith@gmail.com>
2013-02-12 06:31:37 -0600

    Updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	def create
-	  @article = Article.new
-	  @article.title = params[:article][:title]
-	  @article.body = params[:article][:body]
+	  @article = Article.new(params[:article])
+	  #@article = Article.new
+	  #@article.title = params[:article][:title]
+	  #@article.body = params[:article][:body]
 	  @article.save
 	  redirect_to article_path(@article)
 	end
