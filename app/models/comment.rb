51b707c25e8cd8ab258b263af19e297e895a8ccb
tmmtsmith' <tmmtsmith@gmail.com>
2013-02-15 09:57:36 -0600

    Comment has different attributes that can be accessed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   # This prevents mass assignment error
-  attr_accessible :title, :body
+  attr_accessible :author_name, :body
   
   # This deals with foreign key relationships in SQL databases
   belongs_to :article
