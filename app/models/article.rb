e9b6885464745927219ba72f569b8d5484175b01
Tim <tmmtsmith@gmail.com>
2013-02-18 06:28:57 -0600

    Unique tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,7 +12,9 @@ class Article < ActiveRecord::Base
   end
   
   def tag_list=(tags_string)
-	tag_names = tags_string.split(",").collect{|s| s.strip.downcase}
+	self.taggings.destroy_all
+	
+	tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
 
 	tag_names.each do |tag_name|
 		tag = Tag.find_or_create_by_name(tag_name)
