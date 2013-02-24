7d38859db4e042071ad212bc9d66e1bd9c11c827
Tim <tmmtsmith@gmail.com>
2013-02-24 07:24:09 -0600

    Completed

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -4,15 +4,15 @@ class AuthorSessionsController < ApplicationController
 
   def create
     if login(params[:username], params[:password])
-      redirect_back_or_to(articles_path, message: 'Logged in successfully.')
+      redirect_back_or_to(articles_path, message: 'Sign in successful.')
     else
-      flash.now.alert = "Login failed."
+      flash.now.alert = "Sign in failed."
       render action: :new
     end
   end
 
   def destroy
     logout
-    redirect_to(:authors, message: 'Logged out!')
+    redirect_to(:authors, message: 'Sign out successful!')
   end
 end
