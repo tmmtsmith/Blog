de4693ee3308145f66a5bd9d3c80a1c38b72cf1b
Tim <tmmtsmith@gmail.com>
2013-02-24 06:58:02 -0600

    Security update

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Blog::Application.routes.draw do
 	resources :comments
 	resources :tags
 	resources :authors
+	
+	resources :author_sessions, only: [ :new, :create, :destroy ]
+
+	match 'login'  => 'author_sessions#new'
+	match 'logout' => 'author_sessions#destroy'
 end
